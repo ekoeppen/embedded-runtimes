1e827fb96c87e5499e284ca0445d8a818c7a54bc
Eckhart Kőppen <eck@40hz.org>
2017-05-30 07:13:07 +0300

    Remove non-existant IRQ numbers

--- bsps/stm32f411disco/gnarl-bsp/a-intnam.ads
+++ bsps/stm32f411disco/gnarl-bsp/a-intnam.ads
@@ -79,18 +79,6 @@ package Ada.Interrupts.Names is
    --  ADC1 global interrupt
    ADC_Interrupt                     : constant Interrupt_ID := 20;
 
-   --  CAN1 TX interrupts
-   CAN1_TX_Interrupt                 : constant Interrupt_ID := 21;
-
-   --  CAN1 RX0 interrupts
-   CAN1_RX0_Interrupt                : constant Interrupt_ID := 22;
-
-   --  CAN1 RX1 interrupts
-   CAN1_RX1_Interrupt                : constant Interrupt_ID := 23;
-
-   --  CAN1 SCE interrupt
-   CAN1_SCE_Interrupt                : constant Interrupt_ID := 24;
-
    --  EXTI Line[9:5] interrupts
    EXTI9_5_Interrupt                 : constant Interrupt_ID := 25;
 
@@ -139,35 +127,14 @@ package Ada.Interrupts.Names is
    --  USART2 global interrupt
    USART2_Interrupt                  : constant Interrupt_ID := 40;
 
-   --  USART3 global interrupt
-   USART3_Interrupt                  : constant Interrupt_ID := 41;
-
    --  EXTI Line[15:10] interrupts
    EXTI15_10_Interrupt               : constant Interrupt_ID := 42;
 
    --  RTC Alarms (A and B) through EXTI line interrupt
    RTC_Alarm_Interrupt               : constant Interrupt_ID := 43;
 
-   --  USB On-The-Go FS Wakeup through EXTI line interrupt
-   OTG_FS_WKUP_Interrupt             : constant Interrupt_ID := 44;
-
-   --  TIM8 Break interrupt and TIM12 global interrupt
-   TIM8_BRK_TIM12_Interrupt          : constant Interrupt_ID := 45;
-
-   --  TIM8 Update interrupt and TIM13 global interrupt
-   TIM8_UP_TIM13_Interrupt           : constant Interrupt_ID := 46;
-
-   --  TIM8 Trigger and Commutation interrupts and TIM14 global interrupt
-   TIM8_TRG_COM_TIM14_Interrupt      : constant Interrupt_ID := 47;
-
-   --  TIM8 Capture Compare interrupt
-   TIM8_CC_Interrupt                 : constant Interrupt_ID := 48;
-
    --  DMA1 Stream7 global interrupt
    DMA1_Stream7_Interrupt            : constant Interrupt_ID := 49;
-
-   --  FMC global interrupt
-   FMC_Interrupt                     : constant Interrupt_ID := 50;
 
    --  SDIO global interrupt
    SDIO_Interrupt                    : constant Interrupt_ID := 51;
@@ -177,18 +144,6 @@ package Ada.Interrupts.Names is
 
    --  SPI3 global interrupt
    SPI3_Interrupt                    : constant Interrupt_ID := 53;
-
-   --  UART4 global interrupt
-   UART4_Interrupt                   : constant Interrupt_ID := 54;
-
-   --  UART5 global interrupt
-   UART5_Interrupt                   : constant Interrupt_ID := 55;
-
-   --  TIM6 global interrupt, DAC1 and DAC2 underrun error interrupt
-   TIM6_DAC_Interrupt                : constant Interrupt_ID := 56;
-
-   --  TIM7 global interrupt
-   TIM7_Interrupt                    : constant Interrupt_ID := 57;
 
    --  DMA2 Stream0 global interrupt
    DMA2_Stream0_Interrupt            : constant Interrupt_ID := 58;
@@ -204,24 +159,6 @@ package Ada.Interrupts.Names is
 
    --  DMA2 Stream4 global interrupt
    DMA2_Stream4_Interrupt            : constant Interrupt_ID := 62;
-
-   --  Ethernet global interrupt
-   ETH_Interrupt                     : constant Interrupt_ID := 63;
-
-   --  Ethernet Wakeup through EXTI line interrupt
-   ETH_WKUP_Interrupt                : constant Interrupt_ID := 64;
-
-   --  CAN2 TX interrupts
-   CAN2_TX_Interrupt                 : constant Interrupt_ID := 65;
-
-   --  CAN2 RX0 interrupts
-   CAN2_RX0_Interrupt                : constant Interrupt_ID := 66;
-
-   --  CAN2 RX1 interrupts
-   CAN2_RX1_Interrupt                : constant Interrupt_ID := 67;
-
-   --  CAN2 SCE interrupt
-   CAN2_SCE_Interrupt                : constant Interrupt_ID := 68;
 
    --  USB On The Go FS global interrupt
    OTG_FS_Interrupt                  : constant Interrupt_ID := 69;
@@ -244,52 +181,13 @@ package Ada.Interrupts.Names is
    --  I2C3 error interrupt
    I2C3_ER_Interrupt                 : constant Interrupt_ID := 75;
 
-   --  USB On The Go HS End Point 1 Out global interrupt
-   OTG_HS_EP1_OUT_Interrupt          : constant Interrupt_ID := 76;
-
-   --  USB On The Go HS End Point 1 In global interrupt
-   OTG_HS_EP1_IN_Interrupt           : constant Interrupt_ID := 77;
-
-   --  USB On The Go HS Wakeup through EXTI interrupt
-   OTG_HS_WKUP_Interrupt             : constant Interrupt_ID := 78;
-
-   --  USB On The Go HS global interrupt
-   OTG_HS_Interrupt                  : constant Interrupt_ID := 79;
-
-   --  DCMI global interrupt
-   DCMI_Interrupt                    : constant Interrupt_ID := 80;
-
-   --  Rng global interrupt
-   HASH_RNG_Interrupt                : constant Interrupt_ID := 82;
-
    --  FPU global interrupt
    FPU_Interrupt                     : constant Interrupt_ID := 83;
-
-   --  UART 7 global interrupt
-   UART7_Interrupt                   : constant Interrupt_ID := 84;
-
-   --  UART 8 global interrupt
-   UART8_Interrupt                   : constant Interrupt_ID := 85;
 
    --  SPI 4 global interrupt
    SPI4_Interrupt                    : constant Interrupt_ID := 86;
 
    --  SPI 5 global interrupt
    SPI5_Interrupt                    : constant Interrupt_ID := 87;
-
-   --  SPI 6 global interrupt
-   SPI6_Interrupt                    : constant Interrupt_ID := 88;
-
-   --  SAI1 global interrupt
-   SAI1_Interrupt                    : constant Interrupt_ID := 89;
-
-   --  LTDC global interrupt
-   LTDC_Interrupt                    : constant Interrupt_ID := 90;
-
-   --  LTDC global error interrupt
-   LTDC_ER_Interrupt                 : constant Interrupt_ID := 91;
-
-   --  DMA2D global interrupt
-   DMA2D_Interrupt                   : constant Interrupt_ID := 92;
 
 end Ada.Interrupts.Names;
